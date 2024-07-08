a90b846c2e3c4c67a208576e2102fee06872b58e
PDCool64 <p.d.cool@web.de>
2024-07-08 17:36:30 +0200

    Frontend test

---
+++ Frontend/src/pages/TestPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="test">
+      Test
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'TestPage'
+});
+</script>
--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -3,10 +3,10 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/test', component: () => import('pages/TestPage.vue') },
     ]
   },
-
   // Always leave this as last one,
   // but you can also remove it
   {
