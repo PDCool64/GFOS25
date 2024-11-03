6e7c81033f5747a97b2e51c4e6919149aea00ecb
PDCool64 <p.d.cool@web.de>
2024-11-03 15:51:15 +0100

    TokenService now uses Expiration Date

--- Backend/src/main/java/ppj/gfos25/Service/TokenService.java
+++ Backend/src/main/java/ppj/gfos25/Service/TokenService.java
@@ -4,10 +4,12 @@
  */
 package ppj.gfos25.Service;
 
+import java.util.Date;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
+// import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
@@ -17,8 +19,6 @@ import ppj.gfos25.Facades.AccountFacade;
 import jakarta.ejb.Stateless;
 import jakarta.ejb.EJB;
 import jakarta.ejb.LocalBean;
-
-
 
 /**
  *
@@ -31,66 +31,96 @@ public class TokenService {
     public class TokenEmail {
         public String token;
         public String email;
+
         public TokenEmail(String token, String email) {
             this.token = token;
             this.email = email;
         }
     }
 
-    private final String SECRET = "R{,hA:>xYSPLvN~>R\\pz*3feN7EgpM`kfF1e=^22AxnpPI"; // TOPSECRET! Wenn jemand das hier liest, dann sind wir rechtlich am Arsch.
-    private final long DT = 120_000_000; // Token 120_000 Sekunden gültig
-    
+    private final String SECRET = "R{,hA:>xYSPLvN~>R\\pz*3feN7EgpM`kfF1e=^22AxnpPI"; // TOPSECRET! Wenn jemand das hier
+                                                                                     // liest, dann sind wir rechtlich
+                                                                                     // am Arsch.
+    private final long DT = 600_000; // Token 600 Sekunden gültig
+
     @EJB
     private AccountFacade accountFacade;
 
     public String createNewToken(String email) {
-        try {
-            long t = (System.currentTimeMillis() / DT) * DT;
-            Algorithm algorithm = Algorithm.HMAC256(SECRET + t);
-            String token = JWT.create()
+        // try {
+        Date expirationDate = new Date(System.currentTimeMillis());
+        Algorithm algorithm = Algorithm.HMAC256(SECRET);
+        String token = JWT.create()
                 .withIssuer("GFOSProjekt")
                 .withSubject(email)
+                .withExpiresAt(expirationDate)
                 .sign(algorithm);
-            return token;
-        } catch (JWTCreationException exception) {
-            return "JWT-Creation failed.";
-        }
-    }        
-    
+        return token;
+        // } catch (JWTCreationException exception) {
+        // return "JWT-Creation failed.";
+        // } -> Die Exception sollte nie geworfen werden - wenn doch, sollte das geloggt
+        // werden.
+    }
+
     public TokenEmail verifyToken(String token) {
+        Algorithm algorithm = Algorithm.HMAC256(SECRET);
+        Date now = new Date();
         try {
-            long t = (System.currentTimeMillis() / DT) * DT;
-            Algorithm algorithm = Algorithm.HMAC256(SECRET + t);
             JWTVerifier verifier = JWT.require(algorithm)
-                                      .withIssuer("GFOSProjekt")
-                                      .build();
+                    .withIssuer("GFOSProjekt")
+                    .build();
             DecodedJWT jwt = verifier.verify(token);
-            return new TokenEmail(token, jwt.getSubject()); // das gültige Token wieder zurückgeben.
-        } catch (JWTVerificationException ex1){            
-            try { 
-                // Wenn altes Token gerade (innerhalb eines Zeitfensters von
-                // 2*DT) abgelaufen, automatisch um DT Milisekunden erneuern:
-                long t = (System.currentTimeMillis() / DT) * DT - DT;
-                Algorithm algorithm = Algorithm.HMAC256(SECRET + t);
-                JWTVerifier verifier = JWT.require(algorithm)
-                                          .withIssuer("GFOSProjekt")
-                                          .build();
-                DecodedJWT jwt = verifier.verify(token);
-                return new TokenEmail(this.createNewToken(jwt.getSubject()), jwt.getSubject());
-            } catch(JWTVerificationException ex2) {                
-                return new TokenEmail(null, null); // altes Token zu lange (> 2*DT) abgelaufen
+            Date expirationDate = jwt.getExpiresAt();
+            if (now.before(expirationDate)) {
+                return new TokenEmail(token, jwt.getSubject());
             }
+            if (now.getTime() - expirationDate.getTime() < DT) {
+                return new TokenEmail(this.createNewToken(jwt.getSubject()), jwt.getSubject());
+            }
+            return null;
+        } catch (JWTVerificationException exception) {
+            return null;
+        } catch (Exception exception) {
+            return null;
         }
     }
-    
+
+    // public TokenEmail verifyToken(String token) {
+    // try {
+    // long t = (System.currentTimeMillis() / DT) * DT; // rundet die aktuelle Zeit
+    // auf das nächste DT ab
+    // Algorithm algorithm = Algorithm.HMAC256(SECRET + t);
+    // JWTVerifier verifier = JWT.require(algorithm)
+    // .withIssuer("GFOSProjekt")
+    // .build();
+    // DecodedJWT jwt = verifier.verify(token);
+    // return new TokenEmail(token, jwt.getSubject()); // das gültige Token wieder
+    // zurückgeben.
+    // } catch (JWTVerificationException ex1){
+    // try {
+    // // Wenn altes Token gerade (innerhalb eines Zeitfensters von
+    // // 2*DT) abgelaufen, automatisch um DT Milisekunden erneuern:
+    // long t = (System.currentTimeMillis() / DT) * DT - DT;
+    // Algorithm algorithm = Algorithm.HMAC256(SECRET + t);
+    // JWTVerifier verifier = JWT.require(algorithm)
+    // .withIssuer("GFOSProjekt")
+    // .build();
+    // DecodedJWT jwt = verifier.verify(token);
+    // return new TokenEmail(this.createNewToken(jwt.getSubject()),
+    // jwt.getSubject());
+    // } catch(JWTVerificationException ex2) {
+    // return new TokenEmail(null, null); // altes Token zu lange (> 2*DT)
+    // abgelaufen
+    // }
+    // }
+    // }
+
     public Account getUser(String token) {
         try {
             DecodedJWT jwt = JWT.decode(token);
             return accountFacade.getAccountByEmail(jwt.getSubject());
-        }catch(JWTDecodeException exception){
-            return null; 
+        } catch (JWTDecodeException exception) {
+            return null;
         }
     }
-}	
-     
-    
+}
