77faaee21e2ef23225ef32cf89ba0508aa7ca979
PDCool64 <p.d.cool@web.de>
2024-11-25 23:21:38 +0100

    changed color scheme
    Co-authored-by: Philipp1507 <Philipp1507@users.noreply.github.com>
    Co-authored-by: irgendwieCool <irgendwieCool@users.noreply.github.com>

--- Frontend/src/components/PieGraphComponent.vue
+++ Frontend/src/components/PieGraphComponent.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="wrapper">
-        <Pie :data="data" :options="options" />
-    </div>
+	<div class="wrapper">
+		<Pie :data="data" :options="options" />
+	</div>
 </template>
 
 <script setup>
-import { Pie } from 'vue-chartjs';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from "vue-chartjs";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 
 const props = defineProps({
-    data: {
-        type: Object,
-        default: () => ({}),
-    },
-    options: {
-        type: Object,
-        default: () => ({}),
-    },
+	data: {
+		type: Object,
+		default: () => ({}),
+	},
+	options: {
+		type: Object,
+		default: () => ({}),
+	},
 });
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 </script>
 
 <style scoped>
-
 .wrapper {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 }
 </style>
