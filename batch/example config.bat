35a7412ae216d10b0f60dfdc370a396b85326c9e
PDCool64 <p.d.cool@web.de>
2024-12-25 20:50:27 +0100

    added example config

---
+++ batch/example config.bat
@@ -0,0 +1,8 @@
+set glassfish_bin=C:\Users\phili\GlassFish_Server\glassfish\bin
+set project_dir=C:\Users\phili\OneDrive\Desktop\GFOS25
+
+
+set backend_dir=%project_dir%\Backend
+set frontend_dir=%project_dir%\Frontend
+set batch_dir=%project_dir%\batch
+set target_dir=%backend_dir%\target
