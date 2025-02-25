a98e3c42a3c736a7592643f2c7501d160b51261e
PDCool64 <p.d.cool@web.de>
2025-02-25 20:56:16 +0100

    fetch all kunden

--- Frontend/src/stores/kunde.js
+++ Frontend/src/stores/kunde.js
@@ -38,6 +38,17 @@ export const useKundeStore = defineStore("kunde", {
 			console.log(this.kunden);
 			console.log(data);
 		},
+		async fetchAllKunden() {
+			const response = await get_no_data("/kunde/all");
+			const data = await response.json();
+			for (const kunde of data) {
+				if (kunde.kundenstatus == 1) {
+					this.interessenten[String(kunde.id)] = kunde;
+				} else {
+					this.kunden[String(kunde.id)] = kunde;
+				}
+			}
+		},
 	},
 	persist: {
 		enabled: true,
